3e6cef4ffef9ce85fca9359e2cee987e46352bb1
Robert Bivens <robertbivens@my.unt.edu>
2025-05-18 13:07:20 -0500

    Update .config/nvim/lua/config/options.lua

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -3,3 +3,24 @@
 -- Add any additional options here
 
 vim.opt_global.acd = false
+
+vim.o.clipboard = "unnamedplus"
+
+local function paste()
+  return {
+    vim.fn.split(vim.fn.getreg(""), "\n"),
+    vim.fn.getregtype(""),
+  }
+end
+
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = paste,
+    ["*"] = paste,
+  },
+}

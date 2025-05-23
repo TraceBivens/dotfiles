a4157a566e5e069c3725a510f303a7eed2771744
Robert Bivens <robertbivens@my.unt.edu>
2025-05-23 09:39:58 -0500

    Update .config/nvim/lua/config/keymaps.lua

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -4,6 +4,6 @@
 
 vim.keymap.set("i", "jj", "<ESC>", { silent = true })
 
-vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
-vim.keymap.set("n", "<space>x", ":.lua<CR>")
-vim.keymap.set("v", "<space>x", ":lua<CR>")
+-- vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+-- vim.keymap.set("n", "<space>x", ":.lua<CR>")
+-- vim.keymap.set("v", "<space>x", ":lua<CR>")
